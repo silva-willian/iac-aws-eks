7d4f941863c8717e652be1c56a9a80c2892d67e6
Willian da Silva <silva.willian@outlook.com>
2021-10-04 10:21:06 -0300

    Adicionando o script de destroy do cluster

---
+++ destroy-cluster.sh
@@ -0,0 +1,34 @@
+check_sucessful(){
+  if [ $? != 0 ];
+  then
+    echo "Error Execution"
+    exit 1
+  fi
+}
+
+validate_envs() {
+
+  if [ -z "$ENV" ] || [ -z "$ENV_VERSION" ] || [ -z "$CLUSTER_REGION" ] || [ -z "$NAME" ]; 
+  then
+    echo "The parameters ENV, ENV_VERSION, AWS_REGION and NAME are mandatory"
+    exit 1
+  fi
+}
+
+solve_local_variables () {
+  CLUSTER_NAME="${NAME}-${ENV}-${ENV_VERSION}"
+}
+
+destroy_cluster() {
+
+  eksctl delete cluster --region ${CLUSTER_REGION} --name ${CLUSTER_NAME}
+}
+
+validate_envs
+  check_sucessful
+
+solve_local_variables
+  check_sucessful
+
+destroy_cluster
+  check_sucessful
